ee22bcf81d56f05278d12dae3741b46a88005866
davidaodejobi <talk2engineerdavid@gmail.com>
2021-11-04 22:50:43 +0100

    customized the listview cards

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -9,23 +9,23 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-// 2
+
     final ThemeData theme = ThemeData();
-// 3
+
     return MaterialApp(
-// 4c
+
       title: 'Recipe Calculator',
-// 5
+
       theme: theme.copyWith(
         colorScheme: theme.colorScheme.copyWith(
           primary: Colors.grey,
           secondary: Colors.black,
         ),
       ),
-// 6
+
       home: const MyHomePage(title: 'Recipe Calculator'),
     );
   }
@@ -63,11 +63,26 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget buildRecipeCard(Recipe recipe) {
     return Card(
-      child: Column(
-        children: <Widget>[
-          Image(image: AssetImage(recipe.imageUrl)),
-          Text(recipe.label),
-        ],
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage(recipe.imageUrl)),
+            const SizedBox(
+              height: 14.0,
+            ),
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
