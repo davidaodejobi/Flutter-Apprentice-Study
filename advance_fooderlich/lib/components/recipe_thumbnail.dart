cd6d8ad499ba3a440f4f7b960baceea67a29a2b3
davidaodejobi <talk2engineerdavid@gmail.com>
2021-11-28 14:33:49 +0100

    recipe screen added

--- advance_fooderlich/lib/components/recipe_thumbnail.dart
+++ advance_fooderlich/lib/components/recipe_thumbnail.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import '../models/models.dart';
 
 class RecipeThumbnail extends StatelessWidget {
-// 1
   final SimpleRecipe recipe;
   const RecipeThumbnail({
     Key? key,
@@ -10,27 +9,21 @@ class RecipeThumbnail extends StatelessWidget {
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-// 2
     return Container(
       padding: const EdgeInsets.all(8),
-// 3
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-// 4
           Expanded(
-// 5
             child: ClipRRect(
               child: Image.asset(
-                '${recipe.dishImage}',
+                recipe.dishImage,
                 fit: BoxFit.cover,
               ),
               borderRadius: BorderRadius.circular(12),
             ),
           ),
-// 6
           const SizedBox(height: 10),
-// 7
           Text(
             recipe.title,
             maxLines: 1,
--- advance_fooderlich/lib/components/recipes_grid_view.dart
+++ advance_fooderlich/lib/components/recipes_grid_view.dart
@@ -3,7 +3,6 @@ import '../components/components.dart';
 import '../models/models.dart';
 
 class RecipesGridView extends StatelessWidget {
-// 1
   final List<SimpleRecipe> recipes;
   const RecipesGridView({
     Key? key,
@@ -11,22 +10,18 @@ class RecipesGridView extends StatelessWidget {
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-// 2
     return Padding(
       padding: const EdgeInsets.only(
         left: 16,
         right: 16,
         top: 16,
       ),
-// 3
       child: GridView.builder(
-// 4
         itemCount: recipes.length,
-// 5
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
         itemBuilder: (context, index) {
-// 6
           final simpleRecipe = recipes[index];
           return RecipeThumbnail(recipe: simpleRecipe);
         },
