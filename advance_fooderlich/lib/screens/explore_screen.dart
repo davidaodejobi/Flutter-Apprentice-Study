f926afd4865f96981de4c762c43cca13fa88a1be
davidaodejobi <talk2engineerdavid@gmail.com>
2021-11-28 13:19:14 +0100

    explore screen completed

--- advance_fooderlich/lib/screens/explore_screen.dart
+++ advance_fooderlich/lib/screens/explore_screen.dart
@@ -4,25 +4,60 @@ import '../models/models.dart';
 import '../api/mock_fooderlich_service.dart';
 
 class ExploreScreen extends StatelessWidget {
-// 1
   final mockService = MockFooderlichService();
   ExploreScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-// TODO 1: Add TodayRecipeListView FutureBuilder
     return FutureBuilder(
       future: mockService.getExploreData(),
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
-        // TODO: Add Nested List Views
         if (snapshot.connectionState == ConnectionState.done) {
-          final recipes = snapshot.data?.todayRecipes ?? [];
-          return TodayRecipeListView(recipes: recipes);
-        } else {
-          return const Center(
-            child: CircularProgressIndicator(),
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+              const SizedBox(height: 16),
+              FriendPostListView(friendPosts: snapshot.data?.friendPosts ?? []),
+            ],
           );
+        } else {
+          return const Center(child: CircularProgressIndicator());
         }
       },
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   @override
+//   Widget build(BuildContext context) {
+// // TODO 1: Add TodayRecipeListView FutureBuilder
+//     return FutureBuilder(
+//       future: mockService.getExploreData(),
+//       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+//         // TODO: Add Nested List Views
+//         if (snapshot.connectionState == ConnectionState.done) {
+//           final recipes = snapshot.data?.todayRecipes ?? [];
+//           return TodayRecipeListView(recipes: recipes);
+//         } else {
+//           return const Center(
+//             child: CircularProgressIndicator(),
+//           );
+//         }
+//       },
+//     );
+//   }
+// }
